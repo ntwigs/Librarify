8270425dbafa56a1580680b7e19f9283583d0066
Oscar Nordquist <on222bx@student.lnu.se>
2016-12-17 21:13:47 +0100

    #book Can display any cover

--- client/src/components/Book.js
+++ client/src/components/Book.js
@@ -4,7 +4,7 @@ import DisplayBook from './DisplayBook'
 export default class Book extends Component {
   render() {
     return (
-      <DisplayBook />
+      <DisplayBook cover={'http://static.wixstatic.com/media/26f38b_edfdf84993cb45358700f0ca9fed42c2~mv2.jpg'}/> //Url
     )
   }
 }
--- client/src/components/DisplayBook.js
+++ client/src/components/DisplayBook.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 import './DisplayBook.css'
 
 export default class DisplayBook extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
       x: 0,
       y: 0
@@ -24,18 +24,22 @@ export default class DisplayBook extends Component {
     const y = this.state.y
     const x = -this.state.x
 
-    const cubeRotation = {
+    return {
       transform: `translateZ(-1000px) rotateY(${y}deg) rotateX(${x}deg)`
     }
-    return cubeRotation
+  }
+
+  getCoverImage() {
+    return {
+      backgroundImage: `url(${this.props.cover})`
+    }
   }
   
   render() {
-    this.test()
     return (
       <div onMouseMove={this.move} className="cube-container">
         <div className="cube" style={this.getPosition()}>
-          <div className="front"></div>
+          <div className="front" style={this.getCoverImage()}></div>
           <div className="back"></div>
           <div className="right"></div>
           <div className="left"></div>
