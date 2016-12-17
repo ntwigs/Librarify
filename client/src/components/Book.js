12aa190ff4946f87dc08be70137526a62dbd091a
Oscar Nordquist <on222bx@student.lnu.se>
2016-12-17 21:39:18 +0100

    #book renders image from db

--- client/src/components/Book.js
+++ client/src/components/Book.js
@@ -4,7 +4,7 @@ import DisplayBook from './DisplayBook'
 export default class Book extends Component {
   render() {
     return (
-      <DisplayBook cover={'http://static.wixstatic.com/media/26f38b_edfdf84993cb45358700f0ca9fed42c2~mv2.jpg'}/> //Url
+      <DisplayBook cover={this.props.cover}/> //Url
     )
   }
 }
--- client/src/pages/Library.js
+++ client/src/pages/Library.js
@@ -2,19 +2,40 @@ import React, { Component } from 'react'
 import Book from '../components/Book'
 
 export default class Library extends Component {
+  constructor() {
+    super()
+    this.state = {
+      books: []
+    }
+  }
+
+  componentDidMount() {
+    this.getAllBooks()
+  }
+
   getAllBooks() {
     fetch('http://localhost:8000/')
     .then(books => books.json())
-    .then(res => console.log(res))
+    .then(jBooks => {
+      this.setState({
+        books: jBooks
+      })
+    })
+  }
+
+  displayAllBooks() {
+    let bookArray = []
+    for (let i = 0; i < this.state.books.length; i++) {
+      bookArray.push(<Book key={i} cover={this.state.books[i].book_cover} />)
+    }
+    return bookArray
   }
 
 
   render() {
-    this.getAllBooks()
     return (
       <div>
-        <Book />
-        <Book />
+        {this.displayAllBooks()}
       </div>
     )
   }
