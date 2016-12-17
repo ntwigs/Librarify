4e8ff1b5cc17fb6be19ce020276b8d3e48529568
Oscar Nordquist <on222bx@student.lnu.se>
2016-12-17 20:05:23 +0100

    #book animated

--- client/src/components/DisplayBook.js
+++ client/src/components/DisplayBook.js
@@ -2,11 +2,38 @@ import React, { Component } from 'react'
 import './DisplayBook.css'
 
 export default class DisplayBook extends Component {
+  constructor() {
+    super()
+    this.state = {
+      x: 0,
+      y: 0
+    }
+  }
+
+  move = (e) => {
+    const x = (e.nativeEvent.offsetY - 100) / 3.6
+    const y = (e.nativeEvent.offsetX - 100) / 3.6
+
+    this.setState({
+      x: x,
+      y: y
+    })
+  }
+
+  getPosition() {
+    const y = this.state.y
+    const x = -this.state.x
+
+    const cubeRotation = {
+      transform: `translateZ(-1000px) rotateY(${y}deg) rotateX(${x}deg)`
+    }
+    return cubeRotation
+  }
+
   render() {
     return (
-      <div className="cube-container">
-        <div className="origo"></div>
-        <div className="cube">
+      <div onMouseMove={this.move} className="cube-container">
+        <div className="cube" style={this.getPosition()}>
           <div className="front"></div>
           <div className="back"></div>
           <div className="right"></div>
