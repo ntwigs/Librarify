51be24625662a133d07150344b2e6dcb64b1ab9f
Oscar Nordquist <on222bx@student.lnu.se>
2016-12-17 14:47:29 +0100

    #book getable

--- config/DAL.js
+++ config/DAL.js
@@ -2,9 +2,26 @@ import sqlite3 from 'sqlite3'
 
 // TODO: Get all, delete one, create one, edit one
 
-class SQL {
+export default class SQL {
     constructor() {
         this.sql = sqlite3.verbose()
-        this.db = new this.sql.Database('./secret/library')
+        this.db = new this.sql.Database('config/secret/Library')
+    }
+
+    readAll() {
+        return new Promise((res, rej) => {
+            this.db.serialize(() => {
+                this.db.all('SELECT * FROM Books', (err, books) => {
+                    if (err) {
+                        console.log(err)
+                    }
+                    res(books)
+                })
+            })
+        })
+    }
+
+    close() {
+        this.db.close()
     }
 }
--- routes/library.js
+++ routes/library.js
@@ -1,8 +1,17 @@
 import express from 'express'
+import DAL from '../config/DAL'
+
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.send('library')
+  const dal = new DAL()
+  let books
+
+  dal.readAll()
+  .then((e) => books = e)
+  .then(() => dal.close())
+  .then(() => res.send(books))
+
 })
 
 export default router
