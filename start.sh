aab412ee65a916c6b5f4d3a6b48910dca10bfe21
Oscar Nordquist <on222bx@student.lnu.se>
2016-12-25 14:54:06 +0100

    #script api file creation

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ npm install
 echo 'Dependencies installed'
 echo 'Creating DB file with Tables'
 
-sqlite3 config/secret/DerTest <<EOS
+sqlite3 config/secret/Library <<EOS
 	CREATE TABLE Books(
 		book_id Integer Unique Primary Key Not Null,
 		book_title Text Not Null,
@@ -25,6 +25,15 @@ sqlite3 config/secret/DerTest <<EOS
 EOS
 
 echo 'DB done'
+echo 'Creating API file'
+
+echo "export default {
+	CSE: 'CSE-key',
+	API: 'API-key'
+}" >config/secret/Google-Api.js
+
+echo 'API file created'
+
 cd ..
 echo 'Server initialized'
 echo 'Initializing Client'
